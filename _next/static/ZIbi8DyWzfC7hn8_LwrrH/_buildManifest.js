fd9c19f5ff6f5d1048b498332d11cb1d68a7315e
Vladyslav <vlad01403069@gmail.com>
2026-04-08 10:20:39 +0300

    Updates

---
+++ _next/static/ZIbi8DyWzfC7hn8_LwrrH/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/job-board-app-kursova//_next/:path+",
+        "destination": "/job-board-app-kursova/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
